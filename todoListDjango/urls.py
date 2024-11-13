263cad1103f774081bc2871ef2755b67bf8d9492
marcuscabrall <marcus_vinicius.c@hotmail.com>
2024-11-13 13:58:53 -0300

    feat: implement the home route - configure home template

--- todoListDjango/urls.py
+++ todoListDjango/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from todos.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
