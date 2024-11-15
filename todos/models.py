51bfd8cb3a732097343f4ef3b865608b7401613e
marcuscabrall <marcus_vinicius.c@hotmail.com>
2024-11-14 22:24:14 -0300

    feat: implement the Todo models - configure models.py

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,10 @@
+from email.policy import default
+
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100, help_text='Enter a title', null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    deadline = models.DateTimeField(null=False, blank=False)
+    finished_at = models.DateTimeField(null=True)
+    description = models.TextField(help_text='Enter a description')
