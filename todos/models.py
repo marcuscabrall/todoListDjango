657d1e14b34880b65a94c83d84c5f0a5e58e43ab
marcuscabrall <marcus_vinicius.c@hotmail.com>
2024-11-18 23:28:53 -0300

    fix: fix models.py andd todo_list.html

--- todos/models.py
+++ todos/models.py
@@ -1,13 +1,15 @@
-from email.policy import default
-
+from datetime import datetime
 from django.db import models
 
-
 class Todo(models.Model):
-    title = models.CharField(
-        max_length=100, help_text="Enter a title", null=False, blank=False
-    )
+    title = models.CharField(max_length=100, help_text="Enter a title", null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     deadline = models.DateTimeField(null=False, blank=False)
-    finished_at = models.DateTimeField(null=True)
-    description = models.TextField(help_text="Enter a description")
+    finished_at = models.DateTimeField(null=True, blank=True)
+    description = models.CharField(max_length=255, help_text="Enter a description")
+
+    def deadline_as_date(self):
+        return datetime.fromtimestamp(self.deadline).strftime('%d/%m/%Y %H:%M') if self.deadline else None
+
+    def finished_as_date(self):
+        return datetime.fromtimestamp(self.finished_at).strftime('%d/%m/%Y %H:%M') if self.finished_at else None
